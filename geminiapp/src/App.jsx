2ae6a083177076fdf22abd2d878f81faf37c12fc
Kanishk Jain <kanishk2555577@gmail.com>
2025-06-22 19:03:39 +0530

    Initial commit - React.js project

---
+++ geminiapp/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Sidebar } from './components/sidebar/Sidebar'
+import Main from './components/main/Main'
+
+export default function App() {
+
+  return (
+    <>
+      <Sidebar/>
+      <Main/>
+    </>
+  )
+}
+
+
