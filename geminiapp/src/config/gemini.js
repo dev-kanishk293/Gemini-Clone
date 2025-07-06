c4515e95f79268f30d4fae66e642774a6f2746a9
Kanishk Jain <kanishk2555577@gmail.com>
2025-07-06 14:51:18 +0530

    Prepare for deploy

--- geminiapp/src/config/gemini.js
+++ geminiapp/src/config/gemini.js
@@ -1,52 +1,39 @@
-const {
-    GoogleGenerativeAI,
-    HarmCategory,
-    HarmBlockThreshold,
-} = require("@goolge/generative-ai");
+// To run this code you need to install the following dependencies:
+// npm install @google/genai mime
+// npm install -D @types/node
 
-const MODEL_NAME = "gemini-2.0-flash-lite";
-const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
+import {
+  GoogleGenAI,
+} from '@google/genai';
 
-async function runChat(prompt) {
-    const genAI = new GoogleGenerativeAI(API_KEY);
-    const model = genAI.getGenerativeModel({ model: MODEL_NAME});
-
-    const generationConfig = {
-        temperature: 0.5,
-        topK: 1,
-        topP: 1,
-        maxOutputTokens: 2048,
-    };
-
-    const safetySettings = [
+async function main() {
+  const ai = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+  });
+  const config = {
+    responseMimeType: 'text/plain',
+  };
+  const model = 'gemini-2.0-flash-lite';
+  const contents = [
+    {
+      role: 'user',
+      parts: [
         {
-            category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+          text: `INSERT_INPUT_HERE`,
         },
-        {
-            category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        },
-        {
-            category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        },
-        {
-            category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        },
-    ];
+      ],
+    },
+  ];
 
-    const chat = model.startChat({
-        generationConfig,
-        safetySettings,
-        history: [
-        ]
-    });
-
-    const result = await chat.sendMessage(prompt);
-    const response = result.resopnse;
-    console.log(response.text());
+  const response = await ai.models.generateContentStream({
+    model,
+    config,
+    contents,
+  });
+  let fileIndex = 0;
+  for await (const chunk of response) {
+    console.log(chunk.text);
+  }
 }
 
-export default runChat;
+main();
